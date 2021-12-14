4f6546cffe4e5c254106e20c0029bd274a3059db
nyoshida44 <nyoshida44@gmail.com>
2021-12-13 22:38:34 -0500

    Custom Hook for modes and first implementation

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(mode, replace = false) {
+    if (!replace) {
+      setMode(mode);
+      setHistory((prev) => [...prev, mode]);
+    } else {
+      setMode(mode);
+      setHistory((prev) => {
+        prev.pop()
+        return [...prev, mode]
+      })
+    } 
+  }
+
+  function back() {
+    setHistory((prev) => {
+      if (prev.length > 1) {
+        prev.pop(); 
+        setMode(prev[prev.length - 1])
+      }
+      return prev
+    });
+  }
+  return { mode, transition, back};
+}
